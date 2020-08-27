a638788f4945175cc60820d1087de5839b89f18c
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-08-27 06:23:02 -0500

    Linter, extensiones y deploy con Now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Seguimos con react!</h1>, document.getElementById("app"));
+ReactDOM.render(<h1>Seguimos con react!</h1>, document.getElementById('app'))
