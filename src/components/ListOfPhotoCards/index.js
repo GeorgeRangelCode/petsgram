41335324e1b46a792fb2256f7e14900c19f28e28
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-08-28 07:54:23 -0500

    Usando Intersection Observer

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { PhotoCard } from '../PhotoCard'
+import React from "react";
+import { PhotoCard } from "../PhotoCard";
 
 export const ListOfPhotoCards = () => {
   return (
     <ul>
-      {
-        [1, 2, 3].map(id => <PhotoCard key={id} />)
-      }
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((id) => (
+        <PhotoCard key={id} />
+      ))}
     </ul>
-  )
-}
+  );
+};
