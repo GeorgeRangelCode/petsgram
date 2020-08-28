8564fadfde9d0eb2c33104740276530b7271f923
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-08-28 07:12:16 -0500

    useEffect: limpiando eventos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,37 @@
-import React from 'react'
-import { Category } from '../Category'
-import { List, Item } from './styles'
-import { useCategories } from '../../hooks/useCategories'
+import React, { useState, useEffect } from "react";
+import { Category } from "../Category";
+import { List, Item } from "./styles";
+import { useCategories } from "../../hooks/useCategories";
 
 export const ListOfCategories = () => {
-  const { categories } = useCategories()
-  return (
-    <List>
-      {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-      }
+  const { categories } = useCategories();
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200;
+      console.log("onScroll -> newShowFixed", newShowFixed);
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
+
+    document.addEventListener("scroll", onScroll);
+
+    return () => document.removeEventListener("scroll", onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <List className={fixed ? "fixed" : ""}>
+      {categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>
+      ))}
     </List>
-  )
-}
+  );
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+  );
+};
