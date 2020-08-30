c2f287a2e9041433d5399a5377ff8a31cfaf4fe7
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-08-30 15:43:09 -0500

    ¿Qué es Reach Router? Creando la ruta Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { ListOfCategories } from "./components/ListOfCategories";
-import { ListOfPhotoCards } from "./container/ListOfPhotoCards";
 import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
 import { Logo } from "./components/Logo";
 import { GlobalStyle } from "./components/GlobalStyles";
+import { Home } from "./pages/Home";
+import { Router } from "@reach/router";
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search);
@@ -16,10 +16,10 @@ export const App = () => {
       {detailId ? (
         <PhotoCardWithQuery id={detailId} />
       ) : (
-        <>
-          <ListOfCategories />
-          <ListOfPhotoCards categoryId={2} />
-        </>
+        <Router>
+          <Home path="/" />
+          <Home path="/pet/:id" />
+        </Router>
       )}
     </>
   );
