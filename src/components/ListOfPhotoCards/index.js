71e32b91b1f3e4b2c25f9ed21ff8a515b9ca99a6
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-08-30 11:13:23 -0500

    Usando el localStorage para guardar los likes

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -5,7 +5,7 @@ export const ListOfPhotoCards = () => {
   return (
     <ul>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((id) => (
-        <PhotoCard key={id} />
+        <PhotoCard key={id} id={id} />
       ))}
     </ul>
   );
