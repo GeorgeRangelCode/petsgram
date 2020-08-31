008a0da2e44cb941b43fa46dab0c40f82cc1af9a
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-08-31 06:54:37 -0500

    Creando la página Detail

--- src/App.js
+++ src/App.js
@@ -3,24 +3,19 @@ import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
 import { Logo } from "./components/Logo";
 import { GlobalStyle } from "./components/GlobalStyles";
 import { Home } from "./pages/Home";
+import { Detail } from "./pages/Detail";
 import { Router } from "@reach/router";
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get("detail");
-
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {detailId ? (
-        <PhotoCardWithQuery id={detailId} />
-      ) : (
-        <Router>
-          <Home path="/" />
-          <Home path="/pet/:categoryId" />
-        </Router>
-      )}
+      <Router>
+        <Home path="/" />
+        <Home path="/pet/:categoryId" />
+        <Detail path="/detail/:detailId" />
+      </Router>
     </>
   );
 };
