86d42825a8eca4704dc86488d6f0c4dc8e9185de
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-09-01 07:21:47 -0500

    Controlar estado de carga y error al registrar un usuario

--- src/components/UserForm/styles.js
+++ src/components/UserForm/styles.js
@@ -11,6 +11,9 @@ export const Input = styled.input`
   padding: 8px 4px;
   display: block;
   width: 100%;
+  &[disabled] {
+    opacity: 0.3;
+  }
 `;
 
 export const Button = styled.button`
@@ -21,11 +24,21 @@ export const Button = styled.button`
   display: block;
   width: 100%;
   text-align: center;
+  &[disabled] {
+    opacity: 0.3;
+  }
 `;
 
 export const Title = styled.h2`
   font-size: 18px;
   font-weight: 500;
   padding: 18px 0;
+  text-align: center;
+`;
+
+export const Error = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: red;
   text-align: center;
 `;
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -10,13 +10,24 @@ export const NotRegisteredUser = () => {
         return (
           <>
             <RegisterMutation>
-              {(register) => {
+              {(register, { data, loading, error }) => {
                 const onSubmit = ({ email, password }) => {
                   const input = { email, password };
                   const variables = { input };
                   register({ variables }).then(activateAuth);
                 };
-                return <UserForm onSubmit={onSubmit} title="Registrarse" />;
+
+                const errorMessage =
+                  error && "El usuario ya existe o hay algun problema.";
+
+                return (
+                  <UserForm
+                    disabled={loading}
+                    error={errorMessage}
+                    title="Registrarse"
+                    onSubmit={onSubmit}
+                  />
+                );
               }}
             </RegisterMutation>
             <UserForm onSubmit={activateAuth} title="Iniciar Sesión" />
