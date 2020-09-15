a5e2fd708edb6c3f6f332d990a25f42ac94fa2f0
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-09-15 07:09:48 -0500

    Últimos retoques a las rutas de nuestra aplicación

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GlobalStyle } from "./components/GlobalStyles";
 import { Logo } from "./components/Logo";
 import { NavBar } from "./components/NavBar";
@@ -7,38 +7,29 @@ import { Detail } from "./pages/Detail";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
-import { Router } from "@reach/router";
-import Context from "./Context";
-
-const UserLogged = ({ children }) => {
-  return children({ isAuth: false });
-};
+import { NotFound } from "./pages/NotFound";
+import { Redirect, Router } from "@reach/router";
+import { Context } from "./Context";
 
 export const App = () => {
+  const { isAuth } = useContext(Context);
+
   return (
     <>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisteredUser path="/login" />}
+        {!isAuth && <Redirect from="/favs" to="/login" />}
+        {!isAuth && <Redirect from="/user" to="/login" />}
+        {isAuth && <Redirect from="/login" to="/" />}
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path="/favs" />
-              <NotRegisteredUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
       <NavBar />
     </>
   );
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>Esta página no existe 😥</h1>
+    </div>
+  );
+};
